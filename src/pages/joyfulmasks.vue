fe440d017b861e3e2a5b2fb30340918b44f9c55e
Francesco <francesco_lolli@outlook.com>
2025-03-01 04:46:25 +0100

    wip

--- src/pages/joyfulmasks.vue
+++ src/pages/joyfulmasks.vue
@@ -1,82 +1,113 @@
 <template>
+  <div class="d-flex justify-space-around align-center bg-grey-lighten-4 mb-xxl-16">
 
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-test
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-test
-<br/>
-<br/>
-<br/>
+    <v-parallax :aspect-ratio="1" class="bg-white align-center" src="../assets/art/keyArtNoWatermark.jpg" 
+      cover>
+      <div class="d-flex justify-center">
+        <div class="m-0">
+
+          <v-btn href="/joyfulmasks" append-icon="mdi-arrow-right" class="w-sm-25 ml-md-1 my-2" color="primary"
+            text="Our Games" variant="outlined"></v-btn>
+        </div>
+      </div>
+    </v-parallax>
+  </div>
+
+  <div class="bg-white align-center"
+    style="min-height: auto;" cover>
+    <div class="d-flex flex-column">
+      
+
+      <div class="w-100">
+        <div class="d-flex justify-center mx-8 my-8 px-16 py-12">
+          <div>
+            <div class="text-nowrap text-secondary text-h3 font-weight-bold">Behind the Pixels</div>
+            <p class="text-secondary text-h6">
+              We're a group of developers, artists, and sound designers who just love making games. Some of us bring
+              worlds to life with code, others create stunning visuals, and some shape the atmosphere with music and
+              sound. Everyone adds their own touch, and somehow, it all comes together into something special!
+            </p>
+          </div>
+        </div>
+      </div>
+      
+     
+    </div>
+  </div>
 </template>
 
-<script setup>
+<style scoped>
+/* Default for xl screens (≥ 1904px) */
+.responsive-margin {
+  padding-top: 2%;
+}
 
-  //
+/* lg screens (≥ 1264px and < 1904px) */
+@media (max-width: 1550px) {
+  .responsive-margin {
+    padding-top: 5%;
+  }
+}
+
+/* md screens (≥ 960px and < 1264px) */
+@media (max-width: 1264px) {
+  .responsive-margin {
+    padding-top: 10%;
+  }
+}
+
+/* sm screens (≥ 600px and < 960px) */
+@media (max-width: 960px) {
+  .responsive-margin {
+    padding-top: 12%;
+  }
+}
+
+/* xs screens (< 600px) */
+@media (max-width: 600px) {
+  .responsive-margin {
+    padding-top: 15%;
+  }
+}
+
+/* Extra small screens (phones, < 400px) */
+@media (max-width: 400px) {
+  .responsive-margin {
+    padding-top: 20%;
+  }
+}
+</style>
+
+
+<script>
+import { useGoTo } from 'vuetify'
+
+export default {
+  setup() {
+    const goTo = useGoTo()
+    return { goTo }
+  },
+  data() {
+    return {
+      duration: 300,
+      offset: 0,
+      query: '#below',
+      easing: 'easeInOutCubic',
+    }
+  },
+  computed: {
+    options() {
+      return {
+        duration: this.duration,
+        easing: this.easing,
+        offset: this.offset,
+      }
+    },
+  },
+  methods: {
+    onClick() {
+      this.goTo(this.query, this.options)
+    },
+  },
+}
 </script>
--- src/pages/press.vue
+++ src/pages/press.vue
@@ -1,82 +1,138 @@
 <template>
+  <div class="white align-center" style="min-height: auto;" cover>
+    <div class="d-flex flex-column">
 
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-test
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-test
-<br/>
-<br/>
-<br/>
+      <div class="w-100">
+        <div class="d-flex flex-column justify-center mx-8 my-2 px-16 py-12">
+
+          <div class="text-nowrap text-secondary text-h3 font-weight-bold">FACTSHEET</div>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-2">Quick Pitch</div>
+          <p class="text-secondary text-body-1">
+            <span class="text-italic">Joyful Masks</span> is a <span class="font-weight-black text-primary">3D
+              first-person psychological puzzle game with horror elements</span> developed in Unreal Engine 5. Dive
+            into a surreal, ever-shifting world where reality bends, memories unravel, and every puzzle brings you
+            closer to the truth—or deeper into the unknown.
+          </p>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4">The Premise</div>
+          <p class="text-secondary text-body-1">
+            In a university classroom, a radical experiment pushes the limits of neuroscience. Professor Alberto
+            Saggi, a brilliant but haunted researcher, undergoes a dangerous procedure—inducing a deep, drug-induced
+            coma to allow his assistant, Kurt Wagner, to enter his subconscious through an advanced neuro-interactive
+            system.
+            <br /><br />
+            What begins as a controlled study quickly turns into a descent through a fragmented mind, where familiar
+            hallways twist into something living, shadows move on their own, and buried traumas take form. Kurt must
+            navigate psychological puzzles and surreal nightmares to uncover the truth about his mentor’s past—before
+            the experiment spirals out of control.
+          </p>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4">Target Platforms</div>
+          <p class="text-secondary text-body-1">
+            The game is planned for <span class="font-weight-black text-primary">PC (Steam)</span> at launch, with
+            possible future expansions to other platforms.
+          </p>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4">Release & Development</div>
+          <p class="text-secondary text-body-1">
+            Joyful Masks is currently in <span class="font-weight-black text-primary">pre-alpha</span>, with a demo
+            coming soon! We’re planning for a full release in Q4 of 2025 and are fully committed to bringing this
+            experience to life on our own terms. Every detail is being crafted with care to ensure a haunting and
+            immersive journey.
+          </p>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4">Playtime</div>
+          <p class="text-secondary text-body-1">
+            Joyful Masks is a richly atmospheric, story-driven experience. With seven levels, each taking around 30–40
+            minutes, a full playthrough will last <span class="font-weight-black text-primary">approximately four
+              hours</span>.
+            <br /><br />
+            As you navigate the depths of the mind, expect a journey filled with thought-provoking puzzles, quiet
+            tension, and the unraveling of a deeply personal story.
+            <br /><br />
+            Reality is shifting. Can you make sense of it?
+          </p>
+
+          <v-img class="align-end text-white my-10" height="700" src="../assets/art/keyArt.jpg" cover></v-img>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-2">Watch the <span
+              class="font-weight-black text-primary">trailer</span></div>
+
+          <div class="bg-secondary-1 d-flex justify-center align-center font-weight-bold text-subtitle-1"
+            style="height: 30vh;">Coming Soon!</div>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4">Gameplay</div>
+          <p class="text-secondary text-body-1">
+            Joyful Masks is a richly atmospheric, story-driven experience. With seven levels, each taking around 30–40
+            minutes, a full playthrough will last <span class="font-weight-black text-primary">approximately four
+              hours</span>.
+            Step into the mind of Professor Saggi and navigate a world shaped by <span
+              class="font-weight-black text-primary">memories, logic, and hidden truths</span>. Joyful Masks blends
+            first-person puzzle-solving with narrative-driven exploration, challenging you to <span
+              class="font-weight-black text-primary">untangle the subconsciou</span>s one step at a time.
+            <br /><br />
+            Each level is a self-contained, evolving space filled with <span
+              class="font-weight-black text-primary">intricate puzzles</span> that demand observation, reasoning, and
+            creativity. As you move through these shifting environments—sometimes cozy, sometimes eerie—you'll uncover
+            echoes of the professor’s past, <span class="font-weight-black text-primary">piecing together a
+              story</span> buried beneath layers of thought and emotion.
+            <br /><br />
+            With no combat or jump scares, Joyful Masks instead builds tension through discovery, immersion, and <span
+              class="font-weight-black text-primary">the quiet unease of a world that doesn’t always behave as it
+              should</span>. Can you find the patterns, solve the puzzles, and bring clarity to a fractured mind?
+          </p>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4">Aesthetic</div>
+          <p class="text-secondary text-body-1">
+            Joyful Masks draws from a rich <span class="font-weight-black text-primary">Italian atmosphere</span>,
+            blending the warmth of cozy, sunlit spaces with the quiet mystery of overgrown courtyards and forgotten
+            corridors. Inspired by historic universities, aged villas, and the lush greenery of the Italian
+            countryside, our world feels lived-in—filled with history, yet touched by something just out of reach.
+            <br /><br />
+            The contrast between inviting, familiar places and strange, dreamlike distortions creates an experience
+            that is both comforting and intriguing. Every hallway, every puzzle, and every shift in the environment is
+            <span class="font-weight-black text-primary">carefully crafted to evoke a sense of discovery</span>,
+            making the journey through the mind feel as tangible as the real world.?
+          </p>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4">About Us</div>
+          <p class="text-secondary text-body-1">
+            We are <span class="font-weight-black text-primary">Heartfelt Edge Studio</span>, an indie team driven by
+            a love for storytelling, atmosphere, and unique gameplay experiences.
+            <br /><br />
+            What started as a small group with a big idea has grown into a dedicated team of developers, artists, and
+            sound designers, all working together to bring Joyful Masks to life.
+            <br /><br />
+            Our goal is to create games that leave an impact—games that challenge perception, spark curiosity, and
+            stay with you long after you’ve put down the controller. With Joyful Masks, we’re crafting an experience
+            that blends intricate puzzles with deep psychological exploration, inviting players into a world where
+            every detail has meaning.
+            <br /><br />
+            <span class="font-weight-bold text-primary">This is just the beginning for us, and we can’t wait to share
+              what we’ve been working on.</span>
+          </p>
+
+          <div class="text-nowrap text-secondary text-h5 font-weight-bold pt-4 ">Icons and Logos, The Team</div>
+          <div class="bg-primary d-flex justify-center align-center font-weight-bold text-subtitle-1 my-10"
+            style="height: 30vh;">Coming Soon!</div>
+          <div class="bg-primary d-flex justify-center align-center font-weight-bold text-subtitle-1 my-10"
+            style="height: 30vh;">Coming Soon!</div>
+          <div class="bg-primary d-flex justify-center align-center font-weight-bold text-subtitle-1 my-10"
+            style="height: 30vh;">Coming Soon!</div>
+          <v-btn color="primary" class="w-100 mx-auto h-100 py-8 border-md">
+            <p class="text-wrap font-weight-bold text-center text-h4" style="margin: auto;">
+              REQUEST A COPY</p>
+          </v-btn>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script>
 
-  //
 </script>
