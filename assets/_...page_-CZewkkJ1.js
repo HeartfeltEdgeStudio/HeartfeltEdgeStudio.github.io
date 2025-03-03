85bed85cdb2d02effd378e8b5ba36e7f52e34c47
Francesco <francesco_lolli@outlook.com>
2025-03-03 05:43:29 +0100

    wip

---
+++ assets/_...page_-CZewkkJ1.js
@@ -0,0 +1,1 @@
+import{N as e}from"./notFound-aSTeR-q7.js";import{bP as o,a4 as a,F as r,Q as t}from"./index-B1Cc3DAe.js";const m=o({__name:"[...page]",setup(s){return a({title:"Joyful Masks - Error"}),(n,p)=>(t(),r(e))}});export{m as default};
